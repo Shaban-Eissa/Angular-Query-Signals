5837f146a86181ad4dbf9739a3c36686a605e78c
Shaban-Eissa <shabaneissa767@gmail.com>
2025-09-09 14:00:31 +0300

    refactor: update app.component to use RepoListComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RepoListComponent } from "./Components/repo-list/repo-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RepoListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'tanstack-query';
-}
+export class AppComponent { }
